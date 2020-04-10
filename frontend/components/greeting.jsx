4f66d51208ba1bcb270b51acb87af2343547a4e8
danjlee <danjlee0515@gmail.com>
2020-04-10 01:53:16 -0400

    Create dropdown and styling

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -4,6 +4,30 @@ import { Link } from 'react-router-dom';
 class Greeting extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            userMenu: false
+        }
+
+        this.showDropMenu = this.showDropMenu.bind(this);
+        this.hideDropMenu = this.hideDropMenu.bind(this);
+    }
+
+    showDropMenu(e) {
+        e.preventDefault();
+        this.setState({ userMenu: true }, () => {
+            document.addEventListener('click', this.hideDropMenu);
+        });
+    }
+
+    hideDropMenu() {
+        this.setState({ userMenu: false}, () => {
+            document.removeEventListener('click', this.hideDropMenu);
+        });
+    }
+
+    handleUserToggle() {
+        this.setState({ userMenu: !this.state.userMenu });
     }
 
     render() {
@@ -17,11 +41,22 @@ class Greeting extends React.Component {
         const greet = () => (
             <span className="nav-right">
                 <div className="container">
-                    <h2 id="greeting">Hi, {this.props.currentUser.fname}</h2>
-
-                    <div className="container">
-                        <button id="sign-out" onClick={this.props.logout}>Sign out</button>
-                    </div>
+                    <span className="user-toggle" onClick={this.showDropMenu}>
+                        <h2 id="greeting">Hi, {this.props.currentUser.fname}</h2>
+                        <span id="down-arrow">
+                            <img src={window.downarrow} />
+                        </span>
+                    </span>
+                    {this.state.userMenu && (
+                        <div className="container">
+                            <ul className="dropdown">
+                                <li>My Profile</li>
+                                <li>My Dining History</li>
+                                <li>My Saved Restaurants</li>
+                                <li onClick={this.props.logout}>Sign out</li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </span>
         );
