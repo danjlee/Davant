92a37e42fa109228384b3e5856939506bbb27fe6
danjlee <danjlee0515@gmail.com>
2020-06-03 04:35:18 -0400

    fix middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
-const middleware = [thunk];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== "production") {
     const {logger} = require("redux-logger");
